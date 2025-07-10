8abf6875799c038b36c7b9ec4ffad14ffea7414f
xperts-co <xperts.data@outlook.com>
2025-07-10 10:43:39 +0530

    Create sci.js

---
+++ cp/taj-ui-10/sci.js
@@ -0,0 +1,17 @@
+// sci10.js
+
+const sci10Chapters = [
+  { code: 'S1', chapter: 'Light – Reflection and Refraction' },
+  { code: 'S2', chapter: 'The Human Eye and the Colourful World' },
+  { code: 'S3', chapter: 'Chemical Reactions and Equations' },
+  { code: 'S4', chapter: 'Acids, Bases and Salts' },
+  { code: 'S5', chapter: 'Metals and Non-metals' },
+  { code: 'S6', chapter: 'Carbon and its Compounds' },
+  { code: 'S7', chapter: 'Periodic Classification of Elements' },
+  { code: 'S8', chapter: 'Life Processes' },
+  { code: 'S9', chapter: 'Control and Coordination' },
+  { code: 'S10', chapter: 'How do Organisms Reproduce?' },
+  { code: 'S11', chapter: 'Heredity and Evolution' },
+  { code: 'S12', chapter: 'Our Environment' },
+  { code: 'S13', chapter: 'Management of Natural Resources' }
+];
