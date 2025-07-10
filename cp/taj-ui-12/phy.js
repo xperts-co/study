48180acbae6fcacfded44091453837af67aecbd3
xperts-co <xperts.data@outlook.com>
2025-07-10 10:36:06 +0530

    Create phy.js

---
+++ cp/taj-ui-12/phy.js
@@ -0,0 +1,20 @@
+// data.js
+
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+  { code: '1', chapter: 'Electric Charges and Fields' },
+  { code: '2', chapter: 'Electrostatic Potential and Capacitance' },
+  { code: '3', chapter: 'Current Electricity' },
+  { code: '4', chapter: 'Moving Charges and Magnetism' },
+  { code: '5', chapter: 'Magnetism and Matter' },
+  { code: '6', chapter: 'Electromagnetic Induction' },
+  { code: '7', chapter: 'Alternating Current' },
+  { code: '8', chapter: 'Electromagnetic Waves' },
+  { code: '9', chapter: 'Ray Optics and Optical Instruments' },
+  { code: '10', chapter: 'Wave Optics' },
+  { code: '11', chapter: 'Dual Nature of Radiation and Matter' },
+  { code: '12', chapter: 'Atoms' },
+  { code: '13', chapter: 'Nuclei' },
+  { code: '14', chapter: 'Semiconductor Electronics: Materials, Devices and Simple Circuits' }
+];
