7fcb2212fbac5f9701319d91ee6dbc60dbcf3375
xperts-co <xperts.data@outlook.com>
2025-07-10 10:53:01 +0530

    Create ff.js

---
+++ cp/taj-ui-10/ff.js
@@ -0,0 +1,14 @@
+// footprints10.js
+
+const footprints10Chapters = [
+  { code: 'F1', chapter: 'A Triumph of Surgery' },
+  { code: 'F2', chapter: 'The Thief’s Story' },
+  { code: 'F3', chapter: 'The Midnight Visitor' },
+  { code: 'F4', chapter: 'A Question of Trust' },
+  { code: 'F5', chapter: 'Footprints Without Feet' },
+  { code: 'F6', chapter: 'The Making of a Scientist' },
+  { code: 'F7', chapter: 'The Necklace' },
+  { code: 'F8', chapter: 'The Hack Driver' },
+  { code: 'F9', chapter: 'Bholi' },
+  { code: 'F10', chapter: 'The Book That Saved the Earth' }
+];
