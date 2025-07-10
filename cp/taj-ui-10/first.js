a841d7cc1329cbbbc851bc731e885ec4b64a312d
xperts-co <xperts.data@outlook.com>
2025-07-10 10:54:36 +0530

    Create first.js

---
+++ cp/taj-ui-10/first.js
@@ -0,0 +1,26 @@
+// firstflight10.js
+
+const firstFlight10Chapters = [
+  { code: 'FF1', chapter: 'A Letter to God' },
+  { code: 'FF2', chapter: 'Nelson Mandela: Long Walk to Freedom' },
+  { code: 'FF3', chapter: 'Two Stories About Flying' },
+  { code: 'FF4', chapter: 'From the Diary of Anne Frank' },
+  { code: 'FF5', chapter: 'The Hundred Dresses – I' },
+  { code: 'FF6', chapter: 'The Hundred Dresses – II' },
+  { code: 'FF7', chapter: 'Glimpses of India' },
+  { code: 'FF8', chapter: 'Mijbil the Otter' },
+  { code: 'FF9', chapter: 'Madam Rides the Bus' },
+  { code: 'FF10', chapter: 'The Sermon at Benares' },
+  { code: 'FF11', chapter: 'The Proposal' },
+  { code: 'FF12', chapter: 'Dust of Snow' },
+  { code: 'FF13', chapter: 'Fire and Ice' },
+  { code: 'FF14', chapter: 'A Tiger in the Zoo' },
+  { code: 'FF15', chapter: 'How to Tell Wild Animals' },
+  { code: 'FF16', chapter: 'The Ball Poem' },
+  { code: 'FF17', chapter: 'Amanda!' },
+  { code: 'FF18', chapter: 'Animals' },
+  { code: 'FF19', chapter: 'The Trees' },
+  { code: 'FF20', chapter: 'Fog' },
+  { code: 'FF21', chapter: 'The Tale of Custard the Dragon' },
+  { code: 'FF22', chapter: 'For Anne Gregory' }
+];
