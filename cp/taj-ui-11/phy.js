a9c05f333c0db7471bfd93b679d0583b5f0c0a21
xperts-co <xperts.data@outlook.com>
2025-07-10 10:39:33 +0530

    Create phy.js

---
+++ cp/taj-ui-11/phy.js
@@ -0,0 +1,19 @@
+// phy11.js
+
+const phy11Chapters = [
+  { code: 'P1', chapter: 'Physical World' },
+  { code: 'P2', chapter: 'Units and Measurements' },
+  { code: 'P3', chapter: 'Motion in a Straight Line' },
+  { code: 'P4', chapter: 'Motion in a Plane' },
+  { code: 'P5', chapter: 'Laws of Motion' },
+  { code: 'P6', chapter: 'Work, Energy and Power' },
+  { code: 'P7', chapter: 'System of Particles and Rotational Motion' },
+  { code: 'P8', chapter: 'Gravitation' },
+  { code: 'P9', chapter: 'Mechanical Properties of Solids' },
+  { code: 'P10', chapter: 'Mechanical Properties of Fluids' },
+  { code: 'P11', chapter: 'Thermal Properties of Matter' },
+  { code: 'P12', chapter: 'Thermodynamics' },
+  { code: 'P13', chapter: 'Kinetic Theory' },
+  { code: 'P14', chapter: 'Oscillations' },
+  { code: 'P15', chapter: 'Waves' }
+];
