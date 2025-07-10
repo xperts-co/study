c93d4db21907ebd58fb897283802d1a9d958187c
xperts-co <xperts.data@outlook.com>
2025-07-10 10:44:28 +0530

    Create math.js

---
+++ cp/taj-ui-9/math.js
@@ -0,0 +1,19 @@
+// math9.js
+
+const math9Chapters = [
+  { code: 'M1', chapter: 'Number Systems' },
+  { code: 'M2', chapter: 'Polynomials' },
+  { code: 'M3', chapter: 'Coordinate Geometry' },
+  { code: 'M4', chapter: 'Linear Equations in Two Variables' },
+  { code: 'M5', chapter: 'Introduction to Euclid’s Geometry' },
+  { code: 'M6', chapter: 'Lines and Angles' },
+  { code: 'M7', chapter: 'Triangles' },
+  { code: 'M8', chapter: 'Quadrilaterals' },
+  { code: 'M9', chapter: 'Areas of Parallelograms and Triangles' },
+  { code: 'M10', chapter: 'Circles' },
+  { code: 'M11', chapter: 'Constructions' },
+  { code: 'M12', chapter: 'Heron’s Formula' },
+  { code: 'M13', chapter: 'Surface Areas and Volumes' },
+  { code: 'M14', chapter: 'Statistics' },
+  { code: 'M15', chapter: 'Probability' }
+];
