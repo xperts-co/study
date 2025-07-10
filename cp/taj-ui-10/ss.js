cbb01e61f2432161a30fd35ced5e89c392b5323b
xperts-co <xperts.data@outlook.com>
2025-07-10 10:55:30 +0530

    Create ss.js

---
+++ cp/taj-ui-10/ss.js
@@ -0,0 +1,27 @@
+// sst10.js
+
+const sst10Chapters = [
+  // History
+  { code: 'H1', chapter: 'The Rise of Nationalism in Europe' },
+  { code: 'H2', chapter: 'Nationalism in India' },
+  { code: 'H3', chapter: 'The Making of a Global World' },
+  { code: 'H4', chapter: 'The Age of Industrialisation' },
+  { code: 'H5', chapter: 'Print Culture and the Modern World' },
+  { code: 'G1', chapter: 'Resources and Development' },
+  { code: 'G2', chapter: 'Forest and Wildlife Resources' },
+  { code: 'G3', chapter: 'Water Resources' },
+  { code: 'G4', chapter: 'Agriculture' },
+  { code: 'G5', chapter: 'Minerals and Energy Resources' },
+  { code: 'G6', chapter: 'Manufacturing Industries' },
+  { code: 'G7', chapter: 'Lifelines of National Economy' },
+  { code: 'P1', chapter: 'Power Sharing' },
+  { code: 'P2', chapter: 'Federalism' },
+  { code: 'P3', chapter: 'Gender, Religion and Caste' },
+  { code: 'P4', chapter: 'Political Parties' },
+  { code: 'P5', chapter: 'Outcomes of Democracy' },
+  { code: 'E1', chapter: 'Development' },
+  { code: 'E2', chapter: 'Sectors of the Indian Economy' },
+  { code: 'E3', chapter: 'Money and Credit' },
+  { code: 'E4', chapter: 'Globalisation and the Indian Economy' },
+  { code: 'E5', chapter: 'Consumer Rights' }
+];
