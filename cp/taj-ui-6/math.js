02c39a3c9d8f8251e2c968f9fde305c2082a1207
xperts-co <xperts.data@outlook.com>
2025-07-10 10:48:37 +0530

    Update and rename index.html to math.js

---
+++ cp/taj-ui-6/math.js
@@ -0,0 +1,18 @@
+// math6.js
+
+const math6Chapters = [
+  { code: 'M1', chapter: 'Knowing Our Numbers' },
+  { code: 'M2', chapter: 'Whole Numbers' },
+  { code: 'M3', chapter: 'Playing with Numbers' },
+  { code: 'M4', chapter: 'Basic Geometrical Ideas' },
+  { code: 'M5', chapter: 'Understanding Elementary Shapes' },
+  { code: 'M6', chapter: 'Integers' },
+  { code: 'M7', chapter: 'Fractions' },
+  { code: 'M8', chapter: 'Decimals' },
+  { code: 'M9', chapter: 'Data Handling' },
+  { code: 'M10', chapter: 'Mensuration' },
+  { code: 'M11', chapter: 'Algebra' },
+  { code: 'M12', chapter: 'Ratio and Proportion' },
+  { code: 'M13', chapter: 'Symmetry' },
+  { code: 'M14', chapter: 'Practical Geometry' }
+];
