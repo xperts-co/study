3a8a658cd85aef9ee826afb9e3f093a9f7ca8d36
xperts-co <xperts.data@outlook.com>
2025-07-10 10:29:14 +0530

    Create math.js

---
+++ cp/taj-ui-12/math.js
@@ -0,0 +1,16 @@
+// data.js
+
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+  { code: 'PHY_01', chapter: 'Units and Measurements' },
+  { code: 'PHY_02', chapter: 'Motion in a Straight Line' },
+  { code: 'PHY_03', chapter: 'Laws of Motion' },
+  { code: 'CHEM_01', chapter: 'Some Basic Concepts of Chemistry' },
+  { code: 'CHEM_02', chapter: 'Structure of Atom' },
+  { code: 'CHEM_03', chapter: 'Biomolecules' },
+  { code: 'BIO_01', chapter: 'The Living World' },
+  { code: 'BIO_02', chapter: 'Plant Kingdom' },
+  { code: 'ENG_01', chapter: 'Tenses' },
+  { code: 'ENG_02', chapter: 'Articles' }
+];
