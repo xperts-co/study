e8e7cf7071892e65acaffee59f0c0dd0864ef9f3
xperts-co <xperts.data@outlook.com>
2025-07-10 10:18:06 +0530

    Create chem.js

---
+++ cp/taj-ui-12/chem.js
@@ -0,0 +1,16 @@
+// data.js
+
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+  { code: '1', chapter: 'Solutions' },
+  { code: '2', chapter: 'Electrochemistry' },
+  { code: '3', chapter: 'Chemical Kinetics' },
+  { code: '4', chapter: 'd -and f -Block Elements' },
+  { code: '5', chapter: 'Coordination Compounds' },
+  { code: '6', chapter: 'Haloalkanes and Haloarenes' },
+  { code: '7', chapter: 'Alcohols, Phenols and Ethers' },
+  { code: '8', chapter: 'Aldehydes, Ketones and Carboxylic Acids' },
+  { code: '9', chapter: 'Amines' },
+  { code: '10', chapter: 'Biomolecules' }
+];
