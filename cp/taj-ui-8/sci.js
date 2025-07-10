3d50d50a88114ec68c6499edd00541c42afb8fe5
xperts-co <xperts.data@outlook.com>
2025-07-10 10:46:36 +0530

    Create sci.js

---
+++ cp/taj-ui-8/sci.js
@@ -0,0 +1,21 @@
+// sci8.js
+
+const sci8Chapters = [
+  { code: 'S1', chapter: 'Force and Pressure' },
+  { code: 'S2', chapter: 'Friction' },
+  { code: 'S3', chapter: 'Sound' },
+  { code: 'S4', chapter: 'Chemical Effects of Electric Current' },
+  { code: 'S5', chapter: 'Some Natural Phenomena' },
+  { code: 'S6', chapter: 'Light' },
+  { code: 'S7', chapter: 'Synthetic Fibres and Plastics' },
+  { code: 'S8', chapter: 'Materials: Metals and Non-Metals' },
+  { code: 'S9', chapter: 'Coal and Petroleum' },
+  { code: 'S10', chapter: 'Combustion and Flame' },
+  { code: 'S11', chapter: 'Crop Production and Management' },
+  { code: 'S12', chapter: 'Microorganisms: Friend and Foe' },
+  { code: 'S13', chapter: 'Conservation of Plants and Animals' },
+  { code: 'S14', chapter: 'Cell – Structure and Functions' },
+  { code: 'S15', chapter: 'Reproduction in Animals' },
+  { code: 'S16', chapter: 'Reaching the Age of Adolescence' },
+  { code: 'S17', chapter: 'Pollution of Air and Water' }
+];
