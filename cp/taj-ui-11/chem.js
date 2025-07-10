7f1c1fef363fc51231a9da33e4ec54202d5e36bf
xperts-co <xperts.data@outlook.com>
2025-07-10 10:39:59 +0530

    Create chem.js

---
+++ cp/taj-ui-11/chem.js
@@ -0,0 +1,17 @@
+// chem11.js
+
+const chem11Chapters = [
+  { code: 'C1', chapter: 'Some Basic Concepts of Chemistry' },
+  { code: 'C2', chapter: 'Structure of Atom' },
+  { code: 'C3', chapter: 'Classification of Elements and Periodicity in Properties' },
+  { code: 'C4', chapter: 'Chemical Bonding and Molecular Structure' },
+  { code: 'C5', chapter: 'States of Matter: Gases and Liquids' },
+  { code: 'C6', chapter: 'Thermodynamics' },
+  { code: 'C7', chapter: 'Equilibrium' },
+  { code: 'C8', chapter: 'Redox Reactions' },
+  { code: 'C9', chapter: 'The s-Block Element' },
+  { code: 'C10', chapter: 'Some p-Block Elements' },
+  { code: 'C11', chapter: 'Organic Chemistry - Some Basic Principles and Techniques' },
+  { code: 'C12', chapter: 'Hydrocarbons' },
+  { code: 'C13', chapter: 'Environmental Chemistry' }
+];
