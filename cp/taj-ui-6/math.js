deaa518890c91b6ff31c95b90bbb0f8497bd57d4
xperts-co <xperts.data@outlook.com>
2025-07-10 11:39:04 +0530

    Update math.js

--- cp/taj-ui-6/math.js
+++ cp/taj-ui-6/math.js
@@ -4,17 +4,5 @@
 // Each chapter has a unique 'code' and a 'chapter' name.
 const Chapters = [
   { code: 'MAT_01', chapter: 'Knowing Our Numbers' },
-  { code: 'M2', chapter: 'Whole Numbers' },
-  { code: 'M3', chapter: 'Playing with Numbers' },
-  { code: 'M4', chapter: 'Basic Geometrical Ideas' },
-  { code: 'M5', chapter: 'Understanding Elementary Shapes' },
-  { code: 'M6', chapter: 'Integers' },
-  { code: 'M7', chapter: 'Fractions' },
-  { code: 'M8', chapter: 'Decimals' },
-  { code: 'M9', chapter: 'Data Handling' },
-  { code: 'M10', chapter: 'Mensuration' },
-  { code: 'M11', chapter: 'Algebra' },
-  { code: 'M12', chapter: 'Ratio and Proportion' },
-  { code: 'M13', chapter: 'Symmetry' },
-  { code: 'M14', chapter: 'Practical Geometry' }
+  { code: 'MAT_02', chapter: 'Whole Numbers' }
 ];
