ce9a0837daf60bc748be3a41e872f0a509e6f786
xperts-co <xperts.data@outlook.com>
2025-07-10 10:38:34 +0530

    Create math.js

---
+++ cp/taj-ui-11/math.js
@@ -0,0 +1,22 @@
+// math11.js
+
+const math11Chapters = [
+  { code: 'M1', chapter: 'Sets' },
+  { code: 'M2', chapter: 'Relations and Functions' },
+  { code: 'M3', chapter: 'Trigonometric Functions' },
+  { code: 'M4', chapter: 'Principle of Mathematical Induction' },
+  { code: 'M5', chapter: 'Complex Numbers and Quadratic Equations' },
+  { code: 'M6', chapter: 'Linear Inequalities' },
+  { code: 'M7', chapter: 'Permutation and Combination' },
+  { code: 'M8', chapter: 'Binomial Theorem' },
+  { code: 'M9', chapter: 'Sequences and Series' },
+  { code: 'M10', chapter: 'Straight Lines' },
+  { code: 'M11', chapter: 'Conic Sections' },
+  { code: 'M12', chapter: 'Introduction to Three Dimensional Geometry' },
+  { code: 'M13', chapter: 'Limits and Derivatives' },
+  { code: 'M14', chapter: 'Mathematical Reasoning' },
+  { code: 'M15', chapter: 'Statistics' },
+  { code: 'M16', chapter: 'Probability' }
+];
+
+export default math11Chapters;
