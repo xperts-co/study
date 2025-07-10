3e8fa789df225243236754683925f04a8caf35ec
xperts-co <xperts.data@outlook.com>
2025-07-10 12:15:40 +0530

    Update bio.js

--- cp/taj-ui-12/bio.js
+++ cp/taj-ui-12/bio.js
@@ -3,7 +3,7 @@
 // This file contains the list of chapters for the test application.
 // Each chapter has a unique 'code' and a 'chapter' name.
 const chapters = [
-  { code: 'PHY_01', chapter: 'Units and Measurements' },
+  { code: 'PHY_01', chapter: 'Hello F' },
   { code: 'PHY_02', chapter: 'Motion in a Straight Line' },
   { code: 'PHY_03', chapter: 'Laws of Motion' },
   { code: 'CHEM_01', chapter: 'Some Basic Concepts of Chemistry' },
