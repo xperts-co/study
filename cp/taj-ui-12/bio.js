5c6c71e994ed66adec9baa45a908a1b954da32b3
xperts-co <xperts.data@outlook.com>
2025-07-10 10:28:44 +0530

    Create bio.js

---
+++ cp/taj-ui-12/bio.js
@@ -0,0 +1,21 @@
+// data.js
+
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+  { code: '1', chapter: 'Sexual Reproduction in Flowering Plants' },
+  { code: '2', chapter: 'Human Reproduction' },
+  { code: '3', chapter: 'Reproductive Health' },
+  { code: '4', chapter: ' Principles of inheritance and variation' },
+  { code: '5', chapter: 'Molecular Basis of Inheritance' },
+  { code: '6', chapter: 'Evolution' },
+  { code: '7', chapter: 'Human Health and Diseases' },
+  { code: '8', chapter: 'Microbes in Human Welfare' },
+  { code: '9', chapter: 'Biotechnology – Principles and Processes' },
+  { code: '10', chapter: 'Biotechnology and its Applications' }
+  { code: '11', chapter: 'Organisms and Populations' },
+  { code: '12', chapter: 'Ecosystem' },
+  { code: '13', chapter: 'Biodiversity and its Conservation' },
+  { code: '14', chapter: 'Organisms and Populations' },
+  { code: '15', chapter: 'Ecosystem' }
+];
