e38a20333f65efcf32ce8a7244a7fc43322c8cf2
xperts-co <xperts.data@outlook.com>
2025-07-10 10:45:23 +0530

    Create sci.js

---
+++ cp/taj-ui-9/sci.js
@@ -0,0 +1,19 @@
+// sci9.js
+
+const sci9Chapters = [
+  { code: 'S1', chapter: 'Motion' },
+  { code: 'S2', chapter: 'Force and Laws of Motion' },
+  { code: 'S3', chapter: 'Gravitation' },
+  { code: 'S4', chapter: 'Work and Energy' },
+  { code: 'S5', chapter: 'Sound' },
+  { code: 'S6', chapter: 'Matter in Our Surroundings' },
+  { code: 'S7', chapter: 'Is Matter Around Us Pure?' },
+  { code: 'S8', chapter: 'Atoms and Molecules' },
+  { code: 'S9', chapter: 'Structure of the Atom' },
+  { code: 'S10', chapter: 'The Fundamental Unit of Life' },
+  { code: 'S11', chapter: 'Tissues' },
+  { code: 'S12', chapter: 'Diversity of Living Organisms' },
+  { code: 'S13', chapter: 'Why Do We Fall Ill?' },
+  { code: 'S14', chapter: 'Natural Resources' },
+  { code: 'S15', chapter: 'Improvement in Food Resources' }
+];
