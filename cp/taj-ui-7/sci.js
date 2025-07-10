457d29a8fa8cf0169694efb01f47f3e4068e80b5
xperts-co <xperts.data@outlook.com>
2025-07-10 10:47:50 +0530

    Create sci.js

---
+++ cp/taj-ui-7/sci.js
@@ -0,0 +1,20 @@
+// sci7.js
+
+const sci7Chapters = [
+  { code: 'S1', chapter: 'Motion and Time' },
+  { code: 'S2', chapter: 'Electric Current and its Effects' },
+  { code: 'S3', chapter: 'Light' },
+  { code: 'S4', chapter: 'Winds, Storms and Cyclones' },
+  { code: 'S5', chapter: 'Heat' },
+  { code: 'S6', chapter: 'Acids, Bases and Salts' },
+  { code: 'S7', chapter: 'Physical and Chemical Changes' },
+  { code: 'S8', chapter: 'Water: A Precious Resource' },
+  { code: 'S9', chapter: 'Wastewater Story' },
+  { code: 'S10', chapter: 'Soil' },
+  { code: 'S11', chapter: 'Nutrition in Plants' },
+  { code: 'S12', chapter: 'Nutrition in Animals' },
+  { code: 'S13', chapter: 'Respiration in Organisms' },
+  { code: 'S14', chapter: 'Transportation in Animals and Plants' },
+  { code: 'S15', chapter: 'Reproduction in Plants' },
+  { code: 'S16', chapter: 'Forests: Our Lifeline' }
+];
