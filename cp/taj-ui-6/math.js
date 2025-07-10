fbb68ed7b61b54415d5ef6edc87e221dd0ac45d6
xperts-co <xperts.data@outlook.com>
2025-07-10 11:34:32 +0530

    Update math.js

--- cp/taj-ui-6/math.js
+++ cp/taj-ui-6/math.js
@@ -1,5 +1,7 @@
-// math6.js
+// data.js
 
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
 const Chapters = [
   { code: 'MAT_01', chapter: 'Knowing Our Numbers' },
   { code: 'M2', chapter: 'Whole Numbers' },
