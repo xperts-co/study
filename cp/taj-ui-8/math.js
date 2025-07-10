65807fc918c2a0b27a97053e66550125db584585
xperts-co <xperts.data@outlook.com>
2025-07-10 10:45:54 +0530

    Create math.js

---
+++ cp/taj-ui-8/math.js
@@ -0,0 +1,20 @@
+// math8.js
+
+const math8Chapters = [
+  { code: 'M1', chapter: 'Rational Numbers' },
+  { code: 'M2', chapter: 'Linear Equations in One Variable' },
+  { code: 'M3', chapter: 'Understanding Quadrilaterals' },
+  { code: 'M4', chapter: 'Practical Geometry' },
+  { code: 'M5', chapter: 'Data Handling' },
+  { code: 'M6', chapter: 'Squares and Square Roots' },
+  { code: 'M7', chapter: 'Cubes and Cube Roots' },
+  { code: 'M8', chapter: 'Comparing Quantities' },
+  { code: 'M9', chapter: 'Algebraic Expressions and Identities' },
+  { code: 'M10', chapter: 'Visualising Solid Shapes' },
+  { code: 'M11', chapter: 'Mensuration' },
+  { code: 'M12', chapter: 'Exponents and Powers' },
+  { code: 'M13', chapter: 'Direct and Inverse Proportions' },
+  { code: 'M14', chapter: 'Factorisation' },
+  { code: 'M15', chapter: 'Introduction to Graphs' },
+  { code: 'M16', chapter: 'Playing with Numbers' }
+];
