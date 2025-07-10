ae5610ee6d8bbdbe1ede52c863dfd2a4d25182e1
xperts-co <xperts.data@outlook.com>
2025-07-10 10:42:15 +0530

    Create math.js

---
+++ cp/taj-ui-10/math.js
@@ -0,0 +1,19 @@
+// math10.js
+
+const math10Chapters = [
+  { code: 'M1', chapter: 'Real Numbers' },
+  { code: 'M2', chapter: 'Polynomials' },
+  { code: 'M3', chapter: 'Pair of Linear Equations in Two Variables' },
+  { code: 'M4', chapter: 'Quadratic Equations' },
+  { code: 'M5', chapter: 'Arithmetic Progressions' },
+  { code: 'M6', chapter: 'Triangles' },
+  { code: 'M7', chapter: 'Coordinate Geometry' },
+  { code: 'M8', chapter: 'Introduction to Trigonometry' },
+  { code: 'M9', chapter: 'Some Applications of Trigonometry' },
+  { code: 'M10', chapter: 'Circles' },
+  { code: 'M11', chapter: 'Constructions' },
+  { code: 'M12', chapter: 'Areas Related to Circles' },
+  { code: 'M13', chapter: 'Surface Areas and Volumes' },
+  { code: 'M14', chapter: 'Statistics' },
+  { code: 'M15', chapter: 'Probability' }
+];
