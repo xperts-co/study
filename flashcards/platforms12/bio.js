f73369d1ac15bcede9967d4d9b7add81f17e8054
xperts-co <xperts.data@outlook.com>
2025-07-10 13:23:13 +0530

    Create bio.js

---
+++ flashcards/platforms12/bio.js
@@ -0,0 +1,29 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    {
+        chapterName: "Cell: The Unit Of Life",
+        code: "11" // Simple, numerical code
+    },
+    {
+        chapterName: "Chapter 5: Photosynthesis",
+        code: "12"
+    },
+    {
+        chapterName: "Unit 3: The Periodic Table",
+        code: "13"
+    },
+    {
+        chapterName: "World History: The Roman Empire",
+        code: "14"
+    }
+    // To add a new chapter, add a new entry here:
+    // {
+    //   chapterName: "New Chapter Title",
+    //   code: "15"
+    // }
+];
