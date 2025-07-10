cd0f2b219264a87f01940f18ce7f47112b07239d
xperts-co <xperts.data@outlook.com>
2025-07-10 10:49:09 +0530

    Create sci.js

---
+++ cp/taj-ui-6/sci.js
@@ -0,0 +1,20 @@
+// sci6.js
+
+const sci6Chapters = [
+  { code: 'S1', chapter: 'Motion and Measurement of Distances' },
+  { code: 'S2', chapter: 'Light, Shadows and Reflections' },
+  { code: 'S3', chapter: 'Electricity and Circuits' },
+  { code: 'S4', chapter: 'Fun with Magnets' },
+  { code: 'S5', chapter: 'Sorting Materials into Groups' },
+  { code: 'S6', chapter: 'Separation of Substances' },
+  { code: 'S7', chapter: 'Changes Around Us' },
+  { code: 'S8', chapter: 'Air Around Us' },
+  { code: 'S9', chapter: 'Water' },
+  { code: 'S10', chapter: 'Garbage In, Garbage Out' },
+  { code: 'S11', chapter: 'Food: Where Does it Come From?' },
+  { code: 'S12', chapter: 'Components of Food' },
+  { code: 'S13', chapter: 'Getting to Know Plants' },
+  { code: 'S14', chapter: 'Body Movements' },
+  { code: 'S15', chapter: 'The Living Organisms and Their Surroundings' }
+];
+
