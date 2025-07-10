463e7a173319cbd45c538036b9ea25fa0b71d330
xperts-co <xperts.data@outlook.com>
2025-07-10 10:38:18 +0530

    Create bio.js

---
+++ cp/taj-ui-11/bio.js
@@ -0,0 +1,26 @@
+// bio11.js
+
+const bio11Chapters = [
+  { code: 'B1', chapter: 'The Living World' },
+  { code: 'B2', chapter: 'Biological Classification' },
+  { code: 'B3', chapter: 'Plant Kingdom' },
+  { code: 'B4', chapter: 'Animal Kingdom' },
+  { code: 'B5', chapter: 'Morphology of Flowering Plants' },
+  { code: 'B6', chapter: 'Anatomy of Flowering Plants' },
+  { code: 'B7', chapter: 'Structural Organisation in Animals' },
+  { code: 'B8', chapter: 'Cell – The Unit of Life' },
+  { code: 'B9', chapter: 'Biomolecules' },
+  { code: 'B10', chapter: 'Cell Cycle and Cell Division' },
+  { code: 'B11', chapter: 'Transport in Plants' },
+  { code: 'B12', chapter: 'Mineral Nutrition' },
+  { code: 'B13', chapter: 'Photosynthesis in Higher Plants' },
+  { code: 'B14', chapter: 'Respiration in Plants' },
+  { code: 'B15', chapter: 'Plant Growth and Development' },
+  { code: 'B16', chapter: 'Digestion and Absorption' },
+  { code: 'B17', chapter: 'Breathing and Exchange of Gases' },
+  { code: 'B18', chapter: 'Body Fluids and Circulation' },
+  { code: 'B19', chapter: 'Excretory Products and Their Elimination' },
+  { code: 'B20', chapter: 'Locomotion and Movement' },
+  { code: 'B21', chapter: 'Neural Control and Coordination' },
+  { code: 'B22', chapter: 'Chemical Coordination and Integration' }
+];
