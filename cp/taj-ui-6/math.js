cf68496d68a445f06970e676167c0745999eb2e9
xperts-co <xperts.data@outlook.com>
2025-07-11 07:56:05 +0530

    Update math.js

--- cp/taj-ui-6/math.js
+++ cp/taj-ui-6/math.js
@@ -3,14 +3,16 @@
 // This file contains the list of chapters for the test application.
 // Each chapter has a unique 'code' and a 'chapter' name.
 const chapters = [
-  { code: 'PHY_01', chapter: 'Units and Measurements' },
-  { code: 'PHY_02', chapter: 'Motion in a Straight Line' },
-  { code: 'PHY_03', chapter: 'Laws of Motion' },
-  { code: 'CHEM_01', chapter: 'Some Basic Concepts of Chemistry' },
-  { code: 'CHEM_02', chapter: 'Structure of Atom' },
-  { code: 'CHEM_03', chapter: 'Biomolecules' },
-  { code: 'BIO_01', chapter: 'The Living World' },
-  { code: 'BIO_02', chapter: 'Plant Kingdom' },
-  { code: 'ENG_01', chapter: 'Tenses' },
-  { code: 'ENG_02', chapter: 'Articles' }
+  { code: 'PHY_01', chapter: 'Knowing Our Numbers' },
+  { code: 'PHY_02', chapter: 'Whole Numbers' },
+  { code: 'PHY_03', chapter: 'Playing with Numbers' },
+  { code: 'CHEM_01', chapter: 'Basic Geometrical Ideas' },
+  { code: 'CHEM_02', chapter: 'Understanding Elementary Shapes' },
+  { code: 'CHEM_03', chapter: 'Integers' },
+  { code: 'BIO_01', chapter: 'Fractions' },
+  { code: 'BIO_02', chapter: 'Decimals' },
+  { code: 'ENG_01', chapter: 'Data Handling' },
+  { code: 'BIO_02', chapter: 'Mensuration' },
+  { code: 'ENG_01', chapter: 'Algebra' },
+  { code: 'ENG_02', chapter: 'Ratio and Proportion' }
 ];
