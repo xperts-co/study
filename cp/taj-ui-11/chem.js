fbb61e7ed63905df708dca9b4a699468ad7bc404
xperts-co <xperts.data@outlook.com>
2025-07-12 07:47:54 +0530

    Update chem.js

--- cp/taj-ui-11/chem.js
+++ cp/taj-ui-11/chem.js
@@ -1,17 +1,19 @@
-// chem11.js
+// data.js
 
-const Chapters = [
-  { code: 'CHEM_01', chapter: 'Some Basic Concepts of Chemistry' },
-  { code: 'CHEM_02', chapter: 'Structure of Atom' },
-  { code: 'CHEM_03', chapter: 'Classification of Elements and Periodicity in Properties' },
-  { code: 'CHEM_04', chapter: 'Chemical Bonding and Molecular Structure' },
-  { code: 'CHEM_05', chapter: 'States of Matter: Gases and Liquids' },
-  { code: 'CHEM_06', chapter: 'Thermodynamics' },
-  { code: 'CHEM_07', chapter: 'Equilibrium' },
-  { code: 'CHEM_08', chapter: 'Redox Reactions' },
-  { code: 'CHEM_09', chapter: 'The s-Block Element' },
-  { code: 'CHEM_10', chapter: 'Some p-Block Elements' },
-  { code: 'CHEM_11', chapter: 'Organic Chemistry - Some Basic Principles and Techniques' },
-  { code: 'CHEM_12', chapter: 'Hydrocarbons' },
-  { code: 'CHEM_13', chapter: 'Environmental Chemistry' }
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+ { code: 'CHEM_11_01', chapter: 'Some Basic Concepts of Chemistry' },
+  { code: 'CHEM_11_02', chapter: 'Structure of Atom' },
+  { code: 'CHEM_11_03', chapter: 'Classification of Elements and Periodicity in Properties' },
+  { code: 'CHEM_11_04', chapter: 'Chemical Bonding and Molecular Structure' },
+  { code: 'CHEM_11_05', chapter: 'States of Matter: Gases and Liquids' },
+  { code: 'CHEM_11_06', chapter: 'Thermodynamics' },
+  { code: 'CHEM_11_07', chapter: 'Equilibrium' },
+  { code: 'CHEM_11_08', chapter: 'Redox Reactions' },
+  { code: 'CHEM_11_09', chapter: 'The s-Block Element' },
+  { code: 'CHEM_11_10', chapter: 'Some p-Block Elements' },
+  { code: 'CHEM_11_11', chapter: 'Organic Chemistry - Some Basic Principles and Techniques' },
+  { code: 'CHEM_11_12', chapter: 'Hydrocarbons' },
+  { code: 'CHEM_11_13', chapter: 'Environmental Chemistry' }
 ];
