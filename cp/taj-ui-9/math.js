35a2f762743a2ea26c55ba6c09c9493524d9bf8a
xperts-co <xperts.data@outlook.com>
2025-07-12 08:06:49 +0530

    Update math.js

--- cp/taj-ui-9/math.js
+++ cp/taj-ui-9/math.js
@@ -1,19 +1,21 @@
-// math9.js
+// data.js
 
-const math9Chapters = [
-  { code: 'M1', chapter: 'Number Systems' },
-  { code: 'M2', chapter: 'Polynomials' },
-  { code: 'M3', chapter: 'Coordinate Geometry' },
-  { code: 'M4', chapter: 'Linear Equations in Two Variables' },
-  { code: 'M5', chapter: 'Introduction to Euclid’s Geometry' },
-  { code: 'M6', chapter: 'Lines and Angles' },
-  { code: 'M7', chapter: 'Triangles' },
-  { code: 'M8', chapter: 'Quadrilaterals' },
-  { code: 'M9', chapter: 'Areas of Parallelograms and Triangles' },
-  { code: 'M10', chapter: 'Circles' },
-  { code: 'M11', chapter: 'Constructions' },
-  { code: 'M12', chapter: 'Heron’s Formula' },
-  { code: 'M13', chapter: 'Surface Areas and Volumes' },
-  { code: 'M14', chapter: 'Statistics' },
-  { code: 'M15', chapter: 'Probability' }
-];
+// This file contains the list of chapters for the test application.
+// Each chapter has a unique 'code' and a 'chapter' name.
+const chapters = [
+  { code: 'MATH_09_01', chapter: 'Number Systems' },
+  { code: 'MATH_09_02', chapter: 'Polynomials' },
+  { code: 'MATH_09_03', chapter: 'Coordinate Geometry' },
+  { code: 'MATH_09_04', chapter: 'Linear Equations in Two Variables' },
+  { code: 'MATH_09_05', chapter: 'Introduction to Euclid’s Geometry' },
+  { code: 'MATH_09_06', chapter: 'Lines and Angles' },
+  { code: 'MATH_09_07', chapter: 'Triangles' },
+  { code: 'MATH_09_08', chapter: 'Quadrilaterals' },
+  { code: 'MATH_09_09', chapter: 'Areas of Parallelograms and Triangles' },
+  { code: 'MATH_09_10', chapter: 'Circles' },
+  { code: 'MATH_09_11', chapter: 'Constructions' },
+  { code: 'MATH_09_12', chapter: 'Heron’s Formula' },
+  { code: 'MATH_09_13', chapter: 'Surface Areas and Volumes' },
+  { code: 'MATH_09_14', chapter: 'Statistics' },
+  { code: 'MATH_09_15', chapter: 'Probability' },
+  ];
