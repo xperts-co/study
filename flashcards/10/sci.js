d8b9886cd571fc629cd309dd85ecd971a6e05b44
xperts-co <xperts.data@outlook.com>
2025-07-12 09:18:00 +0530

    Create sci.js

---
+++ flashcards/10/sci.js
@@ -0,0 +1,72 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    {
+        chapterName: "Chemical Reactions and Equations",
+        code: "15"
+    },
+    {
+        chapterName: "Acids, Bases and Salts",
+        code: "16"
+    },
+    {
+        chapterName: "Metals and Non-metals",
+        code: "17"
+    },
+    {
+        chapterName: "Carbon and Its Compounds",
+        code: "18"
+    },
+    {
+        chapterName: "Periodic Classification of Elements",
+        code: "19"
+    },
+    {
+        chapterName: "Life Processes",
+        code: "20"
+    },
+    {
+        chapterName: "Control and Coordination",
+        code: "21"
+    },
+    {
+        chapterName: "How do Organisms Reproduce?",
+        code: "22"
+    },
+    {
+        chapterName: "Heredity and Evolution",
+        code: "23"
+    },
+    {
+        chapterName: "Light – Reflection and Refraction",
+        code: "24"
+    },
+    {
+        chapterName: "The Human Eye and the Colourful World",
+        code: "25"
+    },
+    {
+        chapterName: "Electricity",
+        code: "26"
+    },
+    {
+        chapterName: "Magnetic Effects of Electric Current",
+        code: "27"
+    },
+    {
+        chapterName: "Sources of Energy",
+        code: "28"
+    },
+    {
+        chapterName: "Our Environment",
+        code: "29"
+    },
+    {
+        chapterName: "Sustainable Management of Natural Resources",
+        code: "30"
+    }
+];
