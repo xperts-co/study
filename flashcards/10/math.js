037a59cfe1e89cb7b1c77409b52bdb5f07b04aa2
xperts-co <xperts.data@outlook.com>
2025-07-12 09:16:48 +0530

    Update and rename flashcards/platform10 to flashcards/10/math.js

---
+++ flashcards/10/math.js
@@ -0,0 +1,69 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    {
+        chapterName: "Real Numbers",
+        code: "15"
+    },
+    {
+        chapterName: "Polynomials",
+        code: "16"
+    },
+    {
+        chapterName: "Pair of Linear Equations in Two Variables",
+        code: "17"
+    },
+    {
+        chapterName: "Quadratic Equations",
+        code: "18"
+    },
+    {
+        chapterName: "Arithmetic Progressions",
+        code: "19"
+    },
+    {
+        chapterName: "Triangles",
+        code: "20"
+    },
+    {
+        chapterName: "Coordinate Geometry",
+        code: "21"
+    },
+    {
+        chapterName: "Introduction to Trigonometry",
+        code: "22"
+    },
+    {
+        chapterName: "Some Applications of Trigonometry",
+        code: "23"
+    },
+    {
+        chapterName: "Circles",
+        code: "24"
+    },
+    {
+        chapterName: "Constructions",
+        code: "25"
+    },
+    {
+        chapterName: "Areas Related to Circles",
+        code: "26"
+    },
+    {
+        chapterName: "Surface Areas and Volumes",
+        code: "27"
+    },
+    {
+        chapterName: "Statistics",
+        code: "28"
+    },
+    {
+        chapterName: "Probability",
+        code: "29"
+    }
+];
+
