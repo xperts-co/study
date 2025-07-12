8f542d513cdc84daf22e135dc7e57b73d52976b0
xperts-co <xperts.data@outlook.com>
2025-07-12 09:14:14 +0530

    Create sci.js

---
+++ flashcards/9/sci.js
@@ -0,0 +1,68 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    {
+        chapterName: "Matter in Our Surroundings",
+        code: "15"
+    },
+    {
+        chapterName: "Is Matter Around Us Pure?",
+        code: "16"
+    },
+    {
+        chapterName: "Atoms and Molecules",
+        code: "17"
+    },
+    {
+        chapterName: "Structure of the Atom",
+        code: "18"
+    },
+    {
+        chapterName: "The Fundamental Unit of Life",
+        code: "19"
+    },
+    {
+        chapterName: "Tissues",
+        code: "20"
+    },
+    {
+        chapterName: "Diversity in Living Organisms",
+        code: "21"
+    },
+    {
+        chapterName: "Motion",
+        code: "22"
+    },
+    {
+        chapterName: "Force and Laws of Motion",
+        code: "23"
+    },
+    {
+        chapterName: "Gravitation",
+        code: "24"
+    },
+    {
+        chapterName: "Work and Energy",
+        code: "25"
+    },
+    {
+        chapterName: "Sound",
+        code: "26"
+    },
+    {
+        chapterName: "Why Do We Fall Ill?",
+        code: "27"
+    },
+    {
+        chapterName: "Natural Resources",
+        code: "28"
+    },
+    {
+        chapterName: "Improvement in Food Resources",
+        code: "29"
+    }
+];
