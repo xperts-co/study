7d58cbddd6a8c39e03fc6df9ffe863f99ad46419
xperts-co <xperts.data@outlook.com>
2025-07-12 08:59:25 +0530

    Create sci.js

---
+++ flashcards/6/sci.js
@@ -0,0 +1,72 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    {
+        chapterName: "Food: Where Does It Come From?",
+        code: "15"
+    },
+    {
+        chapterName: "Components of Food",
+        code: "16"
+    },
+    {
+        chapterName: "Fibre to Fabric",
+        code: "17"
+    },
+    {
+        chapterName: "Sorting Materials into Groups",
+        code: "18"
+    },
+    {
+        chapterName: "Separation of Substances",
+        code: "19"
+    },
+    {
+        chapterName: "Changes Around Us",
+        code: "20"
+    },
+    {
+        chapterName: "Getting to Know Plants",
+        code: "21"
+    },
+    {
+        chapterName: "Body Movements",
+        code: "22"
+    },
+    {
+        chapterName: "The Living Organisms and Their Surroundings",
+        code: "23"
+    },
+    {
+        chapterName: "Motion and Measurement of Distances",
+        code: "24"
+    },
+    {
+        chapterName: "Light, Shadows and Reflections",
+        code: "25"
+    },
+    {
+        chapterName: "Electricity and Circuits",
+        code: "26"
+    },
+    {
+        chapterName: "Fun with Magnets",
+        code: "27"
+    },
+    {
+        chapterName: "Water",
+        code: "28"
+    },
+    {
+        chapterName: "Air Around Us",
+        code: "29"
+    },
+    {
+        chapterName: "Garbage In, Garbage Out",
+        code: "30"
+    }
+];
