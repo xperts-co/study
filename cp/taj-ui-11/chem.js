57aa68586d01637ef84f42550960ac6459e82c0b
xperts-co <xperts.data@outlook.com>
2025-07-12 07:42:45 +0530

    Update chem.js

--- cp/taj-ui-11/chem.js
+++ cp/taj-ui-11/chem.js
@@ -1,17 +1,17 @@
 // chem11.js
 
 const Chapters = [
-  { code: 'C1', chapter: 'Some Basic Concepts of Chemistry' },
-  { code: 'C2', chapter: 'Structure of Atom' },
-  { code: 'C3', chapter: 'Classification of Elements and Periodicity in Properties' },
-  { code: 'C4', chapter: 'Chemical Bonding and Molecular Structure' },
-  { code: 'C5', chapter: 'States of Matter: Gases and Liquids' },
-  { code: 'C6', chapter: 'Thermodynamics' },
-  { code: 'C7', chapter: 'Equilibrium' },
-  { code: 'C8', chapter: 'Redox Reactions' },
-  { code: 'C9', chapter: 'The s-Block Element' },
-  { code: 'C10', chapter: 'Some p-Block Elements' },
-  { code: 'C11', chapter: 'Organic Chemistry - Some Basic Principles and Techniques' },
-  { code: 'C12', chapter: 'Hydrocarbons' },
-  { code: 'C13', chapter: 'Environmental Chemistry' }
+  { code: 'CHEM_01', chapter: 'Some Basic Concepts of Chemistry' },
+  { code: 'CHEM_02', chapter: 'Structure of Atom' },
+  { code: 'CHEM_03', chapter: 'Classification of Elements and Periodicity in Properties' },
+  { code: 'CHEM_04', chapter: 'Chemical Bonding and Molecular Structure' },
+  { code: 'CHEM_05', chapter: 'States of Matter: Gases and Liquids' },
+  { code: 'CHEM_06', chapter: 'Thermodynamics' },
+  { code: 'CHEM_07', chapter: 'Equilibrium' },
+  { code: 'CHEM_08', chapter: 'Redox Reactions' },
+  { code: 'CHEM_09', chapter: 'The s-Block Element' },
+  { code: 'CHEM_10', chapter: 'Some p-Block Elements' },
+  { code: 'CHEM_11', chapter: 'Organic Chemistry - Some Basic Principles and Techniques' },
+  { code: 'CHEM_12', chapter: 'Hydrocarbons' },
+  { code: 'CHEM_13', chapter: 'Environmental Chemistry' }
 ];
