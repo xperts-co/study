7aeb1771d8f85f376efc35f76bba596de43b4e51
xperts-co <xperts.data@outlook.com>
2025-07-12 08:24:13 +0530

    Update math.js

--- cp/taj-ui-6/math.js
+++ cp/taj-ui-6/math.js
@@ -3,16 +3,18 @@
 // This file contains the list of chapters for the test application.
 // Each chapter has a unique 'code' and a 'chapter' name.
 const chapters = [
-  { code: 'MATH_1', chapter: 'Knowing Our Numbers' },
-  { code: 'MATH_2', chapter: 'Whole Numbers' },
-  { code: 'MATH_3', chapter: 'Playing with Numbers' },
-  { code: 'MATH_4', chapter: 'Basic Geometrical Ideas' },
-  { code: 'MATH_5', chapter: 'Understanding Elementary Shapes' },
-  { code: 'MATH_6', chapter: 'Integers' },
-  { code: 'MATH_7', chapter: 'Fractions' },
-  { code: 'MATH_8', chapter: 'Decimals' },
-  { code: 'MATH_9', chapter: 'Data Handling' },
-  { code: 'MATH_10', chapter: 'Mensuration' },
-  { code: 'MATH_11', chapter: 'Algebra' },
-  { code: 'MATH_12', chapter: 'Ratio and Proportion' }
+  { code: 'MATH_06_01', chapter: 'Knowing Our Numbers' },
+  { code: 'MATH_06_02', chapter: 'Whole Numbers' },
+  { code: 'MATH_06_03', chapter: 'Playing with Numbers' },
+  { code: 'MATH_06_04', chapter: 'Basic Geometrical Ideas' },
+  { code: 'MATH_06_05', chapter: 'Understanding Elementary Shapes' },
+  { code: 'MATH_06_06', chapter: 'Integers' },
+  { code: 'MATH_06_07', chapter: 'Fractions' },
+  { code: 'MATH_06_08', chapter: 'Decimals' },
+  { code: 'MATH_06_09', chapter: 'Data Handling' },
+  { code: 'MATH_06_10', chapter: 'Mensuration' },
+  { code: 'MATH_06_11', chapter: 'Algebra' },
+  { code: 'MATH_06_12', chapter: 'Ratio and Proportion' },
+  { code: 'MATH_06_13', chapter: 'Symmetry' },
+  { code: 'MATH_06_14', chapter: 'Practical Geometry' },
 ];
