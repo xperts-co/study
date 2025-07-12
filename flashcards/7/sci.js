e05f4c3345c08f83b9a95e654b7af410a1926851
xperts-co <xperts.data@outlook.com>
2025-07-12 09:04:38 +0530

    Create sci.js

---
+++ flashcards/7/sci.js
@@ -0,0 +1,81 @@
+// data.js
+
+// This is the MASTER LIST of all chapters.
+// Codes MUST be unique and are used as the ID in the database.
+// Add a new chapter here FIRST, then the teacher can add cards to it.
+
+const flashcardSets = [
+    // Class 7 Science Chapters
+    {
+        chapterName: "Nutrition in Plants",
+        code: "15"
+    },
+    {
+        chapterName: "Nutrition in Animals",
+        code: "16"
+    },
+    {
+        chapterName: "Fibre to Fabric",
+        code: "17"
+    },
+    {
+        chapterName: "Heat",
+        code: "18"
+    },
+    {
+        chapterName: "Acids, Bases and Salts",
+        code: "19"
+    },
+    {
+        chapterName: "Physical and Chemical Changes",
+        code: "20"
+    },
+    {
+        chapterName: "Weather, Climate and Adaptations of Animals to Climate",
+        code: "21"
+    },
+    {
+        chapterName: "Winds, Storms and Cyclones",
+        code: "22"
+    },
+    {
+        chapterName: "Soil",
+        code: "23"
+    },
+    {
+        chapterName: "Respiration in Organisms",
+        code: "24"
+    },
+    {
+        chapterName: "Transportation in Animals and Plants",
+        code: "25"
+    },
+    {
+        chapterName: "Reproduction in Plants",
+        code: "26"
+    },
+    {
+        chapterName: "Motion and Time",
+        code: "27"
+    },
+    {
+        chapterName: "Electric Current and Its Effects",
+        code: "28"
+    },
+    {
+        chapterName: "Light",
+        code: "29"
+    },
+    {
+        chapterName: "Water: A Precious Resource",
+        code: "30"
+    },
+    {
+        chapterName: "Forests: Our Lifeline",
+        code: "31"
+    },
+    {
+        chapterName: "Wastewater Story",
+        code: "32"
+    }
+];
